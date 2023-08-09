2cd9b71bfa60e91475c5b90151434c7b36516fcd
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-08-09 21:53:56 +0900

    forget-password & verify-email comp

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,40 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<x-auth-template title="パスワード再設定">
+    <div id="container" class="w-full flex-center-box my-12 px-4">
+        <div class="w-full max-w-xl px-4 sm:px-16 py-8 sm:py-11 rounded-3xl bg-white flex-center-box flex-col">
+            <a href="{{ route('index') }}">
+                <img src="{{ asset('storage/mieet_plus_logo.png') }}" alt="Mieet Plus" class="w-72">
+            </a>
+            <div class="w-full mt-9 mb-4">
+                <div class="text-center text-2xl font-bold mb-4">
+                    {{ __('パスワード再設定') }}
+                </div>
+            </div>
+            <div class="mb-4 text-sm text-gray-600">
+                {{ __('パスワードをお忘れですか？') }}
+                <br>
+                {{ __('ご登録のメールアドレスを入力してください。') }}
+                <br>
+                {{ __('パスワード再設定用のリンクを送信します。') }}
+            </div>
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+
+            <form method="POST" action="{{ route('password.email') }}" class="w-full">
+                @csrf
+
+                <!-- Email Address -->
+                <div class="my-9">
+                    <x-text-input id="email" class="block mt-1 w-full h-12" placeholder="ご登録のメールアドレス" type="email" name="email" :value="old('email')" required autofocus />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button>
+                        {{ __('パスワード再設定リンクを送信') }}
+                    </x-primary-button>
+                </div>
+            </form>
+        </div>
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</x-auth-template>
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,50 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
-
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
-
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
-
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+<x-auth-template title="メールアドレス確認">
+    <div id="container" class="w-full flex-center-box my-12 px-4">
+        <div class="w-full max-w-xl px-4 sm:px-16 py-8 sm:py-11 rounded-3xl bg-white flex-center-box flex-col">
+            <a href="{{ route('index') }}">
+                <img src="{{ asset('storage/mieet_plus_logo.png') }}" alt="Mieet Plus" class="w-72">
+            </a>
+            <div class="w-full mt-9 mb-4">
+                <div class="text-center text-2xl font-bold mb-4">
+                    {{ __('メールアドレス確認') }}
+                </div>
             </div>
-        </form>
+            <div class="mb-4 text-sm text-gray-600">
+                {{ __('ご登録いただきありがとうございます。') }}
+                <br>
+                {{ __('先ほどメールでお送りしたリンクをクリックして、メールアドレスを確認していただけますか？') }}
+                <br>
+                {{ __('メールが届かない場合は、下部のメールリンク再送信のボタンをクリックしてください。') }}
+            </div>
 
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+            @if (session('status') == 'verification-link-sent')
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ __('新しい確認リンクが、登録時に指定した電子メール アドレスに送信されました。') }}
+                </div>
+            @endif
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
-            </button>
-        </form>
+            <div class="mt-4 flex flex-col items-center justify-center w-full">
+                <form method="POST" action="{{ route('verification.send') }}" class="w-full">
+                    @csrf
+
+                    <div>
+                        <x-primary-button>
+                            {{ __('メールリンク再送信') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+
+                <form method="POST" action="{{ route('logout') }}" class="mt-9">
+                    @csrf
+
+                    <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        {{ __('ログアウト') }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
-</x-guest-layout>
+
+
+
+</x-auth-template>
