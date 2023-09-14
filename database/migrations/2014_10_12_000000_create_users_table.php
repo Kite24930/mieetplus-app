1dee86aa0f873aefbe07c661dd26ced09e78fa93
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-09-14 10:14:42 +0900

    filesystems permission fix beta 2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->tinyInteger('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
-            $table->tinyInteger('fist_login')->default(1);
+            $table->tinyInteger('first_login')->default(1);
         });
     }
 
