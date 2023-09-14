1eb14d98086790d88a2f7147b729ab7697ba1137
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-09-14 14:46:47 +0900

    header fix beta 1

--- resources/views/components/recruit/header.blade.php
+++ resources/views/components/recruit/header.blade.php
@@ -1,5 +1,3 @@
 <header class="fixed top-0 left-0 w-full bg-white flex justify-center py-2 z-500">
-    <a href="{{ route('index') }}">
-        <img src="{{ asset('storage/mieet_plus_logo.png') }}" alt="MIEET Plus" class="h-full w-auto">
-    </a>
+    <a href="{{ route('index') }}"><img src="{{ asset('storage/mieet_plus_logo.png') }}" alt="MIEET Plus" class="h-full w-auto"></a>
 </header>
